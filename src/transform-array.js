337d98fb198c82dcb3a4ccd4ca0c6c74b71e720f
Maxim Novikov <maxnovbel@mail.ru>
2021-03-11 14:35:09 +0300

    fix: fix transform array finally

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,41 +1,35 @@
-module.exports = function transform( arr ) {
-  const cases = [
-    '--discard-next',
-    '--discard-prev',
-    '--double-next',
-    '--double-prev'
-  ];
-  const nArr = [];
-
-  if ( arr instanceof Array ) {
-
-    function check(elem, cases) {
-      return cases.indexOf(String(elem)) < 0;
-    }
-
-    for(let i = 0; i < arr.length; i++) {
-      if(check(arr[i] , cases)) {
-        nArr.push(arr[i])
-      } else {
-        switch (arr[i]) {
-          case cases[0]:
-            i+=2;
-            break;
-          case cases[1]:
-            nArr[nArr.length - 1] === arr[i - 1] ? nArr.pop() : false;
-            break;
-          case cases[2]:
-            nArr.push(arr[i + 1]);
-            break;
-          case cases[3]:
-              nArr.push(arr[i - 1]);
-            break;
-        }
+module.exports = function transform(arr) {
+  const oldArr = [...arr];
+  const newArr = [];
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < oldArr.length; i++) {
+      switch (oldArr[i]) {
+        case "--discard-next":
+          if (i < oldArr.length) {
+            newArr.push(undefined);
+            i += 1;
+          }
+          break;
+        case "--discard-prev":
+          if (i > 0) {
+            newArr.pop();
+          }
+          break;
+        case "--double-next":
+          if (i < oldArr.length - 1) {
+            newArr.push(oldArr[i + 1]);
+          }
+          break;
+        case "--double-prev":
+          if (i > 0) {
+            newArr.push(newArr[newArr.length - 1]);
+          }
+          break;
+        default:
+          newArr.push(oldArr[i]);
       }
-    };
-    return nArr.filter(i => check(i, cases) && i !== undefined)
-
-  } else {
-    throw new Error(`${arr} is not an Array`)
+    }
+    return newArr.filter((item) => item !== undefined);
   }
+  throw new Error("not array");
 };
